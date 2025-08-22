00d788cd6eb6f21da7869f089ef81326499c311e
Max <132036976+This-is-Many@users.noreply.github.com>
2025-08-22 21:59:23 +0500

    skoborodka

--- ShatteredFate.cs
+++ ShatteredFate.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria.ModLoader;
 
 namespace ShatteredFate
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class ShatteredFate : Mod
 	{
-
+		public const string VanillaTexture = "Terraria/Images/";
+		public const string ExtrasPath = "ShatteredFate/Extras/";
+		public const string BlankTexture = "ShatteredFate/Extras/Invisible";
+		public const string MagicPixel = "ShatteredFate/Extras/MagicPixel";
 	}
 }
