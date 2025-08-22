65e025690fca3078426e217e89e85c9e7c80b5e4
Max <132036976+This-is-Many@users.noreply.github.com>
2025-08-22 21:58:49 +0500

    skoborodka

---
+++ Content/Buffs/Debuffs/AbilityCooldown.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShatteredFate.Content.Buffs.Debuffs
+{
+	public class AbilityCooldown : ModBuff
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.debuff[Type] = true;
+			Main.buffNoSave[Type] = false;
+			BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
+		}
+	}
+}
