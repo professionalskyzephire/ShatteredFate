5ae31b97c830d0d9766b337f96353cf2beb9f736
Max <132036976+This-is-Many@users.noreply.github.com>
2025-08-23 21:23:14 +0500

    skoborodka fix

---
+++ Content/Buffs/Debuffs/Cooldown_OldFrypan.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShatteredFate.Content.Buffs.Debuffs
+{
+	public class Cooldown_OldFrypan : ModBuff
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.debuff[Type] = true;
+			Main.buffNoSave[Type] = false;
+			BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
+		}
+	}
+}
