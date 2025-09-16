0a6acc1f51dc5aaca93291586ebd359d26116971
HoCha113 <2188704314@qq.com>
2025-09-16 23:36:41 +0800

    Fix Music

---
+++ Common/SFClientConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace ShatteredFate.Common;
+
+public class SFClientConfig : ModConfig
+{
+    [DefaultValue(true)]
+    public bool MusicReplacementsActive { get; set; }
+    public override ConfigScope Mode => ConfigScope.ClientSide;
+}
