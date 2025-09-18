d4edb29bb06a8dd7b8a8b6c25f56a427ef32f22e
N0t_UN_Owen <70080548+Unknowneth@users.noreply.github.com>
2025-09-18 23:39:35 +0800

    Added gem staff cooldown debuffs

---
+++ Content/Buffs/Debuffs/DiamondStaffCooldown.cs
@@ -0,0 +1,17 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+
+namespace ShatteredFate.Content.Buffs.Debuffs
+{
+	public class DiamondStaffCooldown : ModBuff
+	{
+		public override void SetStaticDefaults() {
+			Main.debuff[Type] = true;
+			Main.buffNoSave[Type] = true;
+			BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
+		}
+	}
+}
