43a4d61d883d35a45f021c2b68b699e1a20cf70c
N0t_UN_Owen <70080548+Unknowneth@users.noreply.github.com>
2025-09-29 16:46:24 +0800

    Fixed SFDevItem rarity color being green

--- Content/Rarities/SFDevItem.cs
+++ Content/Rarities/SFDevItem.cs
@@ -5,7 +5,8 @@ namespace ShatteredFate.Content.Rarities
 {
 	public class SFDevItem : ModRarity
 	{
-		public override Color RarityColor => Color.Lerp(new Color(46, 189, 84), Color.White, (float)System.Math.Sin(Terraria.Main.GlobalTimeWrappedHourly * MathHelper.Pi));
+		public override Color RarityColor => Color.Lerp(new Color(246, 189, 84), Color.White, (float)System.Math.Sin(Terraria.Main.GlobalTimeWrappedHourly * MathHelper.Pi));
 		public override int GetPrefixedRarity(int offset, float valueMult) => Type;
 	}
+
 }
