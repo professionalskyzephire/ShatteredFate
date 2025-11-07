ec9ca016c030d2b9d375e7cd493caee409f5fb5d
N0t_UN_Owen <70080548+Unknowneth@users.noreply.github.com>
2025-11-07 16:50:00 +0800

    Added a Debuff GlobalNPC

--- Common/SFDebuffNPC.cs
+++ Common/SFDebuffNPC.cs
@@ -2,6 +2,7 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ModLoader;
 using System.Collections.Generic;
+using System;
 
 namespace ShatteredFate.Common
 {
@@ -9,11 +10,16 @@ namespace ShatteredFate.Common
 	{
 		public override bool InstancePerEntity => true;
 		public Dictionary<int, int> stackingDoTs = new();
+		public int damage = 0;
+		public int rate = 0;
 		public override void ResetEffects(NPC npc) {
+			damage = 0;
+			rate = 0;
 			foreach(int i in stackingDoTs.Keys) if(!npc.HasBuff(i)) stackingDoTs.Remove(i);
 		}
-		public override void UpdateLifeRegen(NPC npc, ref int damage) {
-			foreach(int i in stackingDoTs.Values) npc.lifeRegen -= i;
+		public override void UpdateLifeRegen(NPC npc, ref int dot) {
+			dot += damage;
+			npc.lifeRegen -= rate;
 		}
 	}
 }
